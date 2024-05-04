5990f57148203bb51b10f98bf307cea74ef08528
Cannondian <herrerame3@gmail.com>
2024-05-04 00:29:36 -0500

    Add: Whitebox Area for Part 0 and Vertical Slice

---
+++ Assets/Code/Elevator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Rigidbody elevator_rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        elevator_rb = GetComponent<Rigidbody>();
+        elevator_rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            elevator_rb.useGravity = true;
+        }
+    }
+}
