74f772c766699d324c1a8e6ea1863b2118c7e16a
Jack Burkhardt <mail@jackburkhardt.com>
2024-05-11 14:43:46 -0500

    flashlight, more mechanics in level

---
+++ Assets/Code/FlashlightControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code
+{
+    [RequireComponent(typeof(Light))]
+    public class FlashlightControl : MonoBehaviour
+    {
+        [SerializeField] private Light light;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                light.enabled = !light.enabled;
+            }
+        }
+    }
+}
